67bf7e2a1aa44bfac7571be64ab0c1189def30ce
RajuRk <rajurk044@gmail.com>
2022-07-09 16:38:25 +0530

    first commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+
+function Home() {
+  return (
+    <>
+     <Navbar/>
+     <Header/>
+    </>
+  )
+}
+
+export default Home
